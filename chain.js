65870e39429842c7394a6154868ecd81184a8c7e
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-09-30 06:49:39 +0600

    added ES-6 practice part2 chain

---
+++ chain.js
@@ -0,0 +1,18 @@
+const obj = {
+    name : 'Rafi imrose',
+    age : 22,
+}
+// console.log(obj.age)
+
+const obj1 = {
+    name : ' rafi ',
+    age : 22,
+    address : {
+        state:"kuril moyshanbari road",
+        Road :{
+            no: 40,
+            house : "172/2,shaila monjil"
+        }
+    }
+}
+console.log(obj1.address.Road.house)
